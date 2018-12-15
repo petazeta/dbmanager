ea845c93c518ee9c968dbf6c663de6a20d70ccb6
alberto <melchorherrera@gmail.com>
2018-12-15 22:30:20 +0100

    error

--- includes/config.sample.php
+++ includes/config.sample.php
@@ -1,8 +1,11 @@
 <?php
 // Database user
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'host');
 define('DB_DATABASENAME', 'dbname');
-define('DB_USERNAME', 'uname');
-define('DB_USERPWD', 'pssword');
+define('DB_USERNAME', 'username');
+define('DB_USERPWD', 'password');
 define('DB_CHARSET', 'utf8mb4');
+
+// For when we want split databases by prefix
+define('DB_REMOVE_PREFIX', false);
 ?>
